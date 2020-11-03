28a9ff89ab7c166ef0e5cc0d7ae45938ff481da0
Ray Xu <megrxu@gmail.com>
2020-11-03 15:12:25 +0800

    [set-4] set-4 finished

--- set-4/tests/challenge_32.rs
+++ set-4/tests/challenge_32.rs
@@ -5,6 +5,7 @@ use set_4::challenge_28_sha1_mac::sha1;
 use set_4::challenge_30_md4_mac::md4;
 use set_4::challenge_31_hmac::hmac;
 use std::cmp::Ordering;
+use std::iter;
 
 #[test]
 fn test_hmac() {
@@ -15,21 +16,13 @@ fn test_hmac() {
 fn insecure_compare(a: &[u8], b: &[u8]) -> (bool, usize) {
     let res = a.iter().zip(b.iter()).fold((true, 0), |res, (x, y)| match (res.0, x == y) {
         (false, _) => res,
-        (true, c) => (c, res.1 + 50),
+        (true, c) => (c, res.1 + 5),
     });
-    (res.0 && a.len() == b.len(), res.1)
+    (res.0 && a.len() == b.len(), res.1 + rand!(choose 0..8))
 }
 
 #[test]
-fn test_compare() {
-    assert_eq!(insecure_compare(b"abc", b"abc"), (true, 150));
-    assert_eq!(insecure_compare(b"ab", b"abc"), (false, 100));
-    assert_eq!(insecure_compare(b"abc", b"abd"), (false, 150));
-    assert_eq!(insecure_compare(b"abcdefghijklmn", b"abcdefghijklmm"), (false, 700));
-}
-
-#[test]
-fn exploit_timing_leak() {
+fn exploit_less_timing_leak() {
     let key = rand!(16);
     let file = rand!(rand!(choose 100..1000));
     let target = hmac(sha1, &key, &file, 64);
@@ -50,5 +43,28 @@ fn exploit_timing_leak() {
             .0;
         mutation[i] = byte;
     }
+    // the first method breaks.
+    assert_ne!(mutation, target);
+
+    for i in 0..20 {
+        let byte = (0..0xff)
+            .map(|n| {
+                mutation[i] = n;
+                let r = insecure_compare(&mutation, &target).0;
+                let m = (0..10)
+                    .map(|_| insecure_compare(&mutation, &target))
+                    .fold(0, |res, (_, k)| res + k);
+                (n, (r, m))
+            })
+            .max_by(|&(_, (a_res, a)), (_, (b_res, b))| match (a_res, b_res, a.cmp(b)) {
+                (false, false, _) => a.cmp(b),
+                (false, true, _) => Ordering::Less,
+                (true, _, _) => Ordering::Greater,
+            })
+            .unwrap()
+            .0;
+        mutation[i] = byte;
+    }
+    // by many times.
     assert_eq!(mutation, target);
 }
