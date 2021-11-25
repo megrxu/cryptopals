7aedbe0e91fabcbd51732be821c663e79a69a316
Ray Xu <megrxu@gmail.com>
2021-11-25 14:37:11 +0800

    [set-05] challenge-37

--- set-5/tests/challenge_37.rs
+++ set-5/tests/challenge_37.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use set_2::rand;
-use set_5::challenge_33_dh::uinf;
+use set_4::challenge_28_sha1_mac::sha1;
+use set_5::challenge_33_dh::NIST_P;
 use set_5::challenge_36_srp::*;
 
 #[test]
@@ -8,10 +9,13 @@ fn test_srp_with_some_keys() {
     let mut instance = SRPInstance::init(&rand!(16), &rand!(16));
     let mut client = instance.client();
     let mut server = instance.server();
+
+    // Send N
     // C->S
     // Send I, A=g**a % N (a la Diffie Hellman)
-    let packet = (Some(uinf::from_bytes_be(&[0])), client.email.to_vec());
+    let packet = (Some(NIST_P.clone()), client.email.to_vec());
     instance.handle(&mut client, &mut server, SRPWire::ClientToServer, packet);
+
     // S->C
     // Send salt, B=kv + g**b % N
     let packet = (Some(server.public_key()), server.salt.to_vec());
@@ -19,6 +23,8 @@ fn test_srp_with_some_keys() {
 
     // C->S
     // Token challenge
-    let packet = (None, client.token.clone().unwrap());
+    let token = sha1(&[0]); // Fake Token
+    let packet = (None, token);
     let status = instance.handle(&mut client, &mut server, SRPWire::ClientToServer, packet);
+    assert_eq!(status, SRPStatus::Success);
 }
